c9be8d4069b09aa334623bb3ee9cea6ca323250b
Aditya Garg <99389967+Adi-11garg@users.noreply.github.com>
2022-03-28 02:24:43 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "workshop-sg" {
 #Create instance host in US-EAST-2A Public subnet
 
 resource "aws_instance" "workshop-1a" {
-    ami = "ami-0fb653ca2d3203ac1"
+    ami = "ami-0851b76e8b1bce90b"
     instance_type = "t2.micro"
     key_name = aws_key_pair.ssh-key.key_name
     associate_public_ip_address = true 
@@ -53,7 +53,7 @@ resource "aws_instance" "workshop-1a" {
 #Create instance host in US-EAST-2B Public subnet
 
 resource "aws_instance" "workshop-1b" {
-    ami = "ami-0fb653ca2d3203ac1"
+    ami = "ami-0851b76e8b1bce90b"
     instance_type = "t2.micro"
     key_name = aws_key_pair.ssh-key.key_name
     associate_public_ip_address = true 
