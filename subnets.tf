d330a41e01fa2696c9049bf3489b56281641e9fb
Aditya Garg <99389967+Adi-11garg@users.noreply.github.com>
2022-03-28 02:22:49 +0530

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -3,7 +3,7 @@
 resource "aws_subnet" "workshop1-public-1a" {
     vpc_id = aws_vpc.workshop1-vpc.id
     cidr_block = "10.0.1.0/24"
-    availability_zone = "us-east-2a"
+    availability_zone = "ap-south-1a"
     map_public_ip_on_launch = "true"
     tags = {
         Name = "Workshop Public Subnet - 1A"
@@ -14,7 +14,7 @@ resource "aws_subnet" "workshop1-public-1a" {
 resource "aws_subnet" "workshop1-public-1b" {
     vpc_id = aws_vpc.workshop1-vpc.id
     cidr_block = "10.0.2.0/24"
-    availability_zone = "us-east-2b"
+    availability_zone = "ap-south-1b"
     map_public_ip_on_launch = "true"
     tags = {
         Name = "Workshop Public Subnet - 1B"
@@ -27,7 +27,7 @@ resource "aws_subnet" "workshop1-public-1b" {
 resource "aws_subnet" "workshop1-private-1a" {
     vpc_id = aws_vpc.workshop1-vpc.id
     cidr_block = "10.0.3.0/24"
-    availability_zone = "us-east-2a"
+    availability_zone = "ap-south-1a"
     map_public_ip_on_launch = "false"
     tags = {
         Name = "Workshop Private Subnet - 1A"
@@ -38,7 +38,7 @@ resource "aws_subnet" "workshop1-private-1a" {
 resource "aws_subnet" "workshop1-private-1b" {
     vpc_id = aws_vpc.workshop1-vpc.id
     cidr_block = "10.0.4.0/24"
-    availability_zone = "us-east-2b"
+    availability_zone = "ap-south-1b"
     map_public_ip_on_launch = "false"
     tags = {
         Name = "Workshop Private Subnet - 1B"
